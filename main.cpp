a3120ed76f4798f6a6ddcadfb2153bcbb4ee30dd
ronentgreat <ronenh53@gmail.com>
2021-09-15 19:50:40 +0000

    Begin

--- main.cpp
+++ main.cpp
@@ -12,8 +12,13 @@
 using namespace std;
 
 int main() {
-    cout << "Enter the conversion you want to perform."
-    cout << "1. "
+    cout << "Enter the conversion you want to perform." << endl;
+    cout << "1.Celsius to Kelvin"<< endl;
+    cout << "2.Celsius to Fahrenheit"<< endl;
+    cout << "3.Kelvin to Celsius"<< endl;
+    cout << "4.Kelvin to Fahrenheit"<< endl;
+    cout << "5.Fahrenheit to Celsius"<< endl;
+    cout << "6.Fahrenheit to Kelvin"<< endl;
 
 
 
