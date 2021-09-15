85f57798c39318c972cc4a7ae817111cbb5be774
ronentgreat <ronenh53@gmail.com>
2021-09-15 19:46:28 +0000

    Begin

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+/* A program that converts;
+ 1. Celsius to Kelvin
+ 2. Celsius to Fahrenheit
+ 3. Kelvin to Celsius
+ 4. Kelvin to Fahrenheit
+ 5. Fahrenheit to Celsius
+ 6. Fahrenheit to Kelvin
+
+ */
+
+using namespace std;
+
+int main() {
+    cout << "Enter the conversion you want to perform."
+    cout << "1. "
+
+
+
+    return 0;
+}
