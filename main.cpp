fa78dc46eb4478a09c5c9d58168ec7c4097ead26
ronentgreat <ronenh53@gmail.com>
2021-09-15 20:57:28 +0000

    Good night

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@
 using namespace std;
 
 int main() {
-    int choose;
-    double celsius, kelvin, fahrenheit;
+    int choice;
+    double temperature;
 
     cout << "Enter the conversion you want to perform." << endl;
     cout << "1.Celsius to Kelvin"<< endl;
@@ -24,11 +24,29 @@ int main() {
     cout << "6.Fahrenheit to Kelvin"<< endl;
 
     cout << ">> ";
-    cin >> choose;
+    cin >> choice;
 
-    switch (choose) {
-        
+    if(choice == 1) {
+        cout << "Enter temperature in Celsius: ";
+        cin >> temperature;
+        double celsiusToKelvin = temperature + 273.15;
+        cout << temperature << "\370C in Kelvin is " << celsiusToKelvin << "K" << endl;
     }
+    else if(choice == 2){
+        cout << "Enter temperature in Celsius: ";
+        cin >> temperature;
+        double celsiusToFahrenheit = (temperature * (9.0 / 5.0) + 32.0);
+        cout << temperature << "\370C in Fahrenheit is " << celsiusToFahrenheit << "\370F" << endl;
+    }
+
+    else if(choice == 3){
+        cout << "Enter temperature in Kelvin: ";
+        cin >> temperature;
+        double kelvinToCelsius = temperature - 273.15;
+        cout << temperature << "K in Celsius is " << kelvinToCelsius << "\370C" << endl;
+
+    }
+    // Continue with 4.
 
 
 
