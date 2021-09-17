937895c2ca419a53d9613fe7e2a52de7daa71d82
ronentgreat <ronenh53@gmail.com>
2021-09-17 11:42:08 +0000

    added a goto statement.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main() {
     int choice;
     double temperature;
 
-    N:
+    N: // After the calculation the program will jump to this part
     cout << "Enter the conversion you want to perform." << endl;
     cout << "1.Celsius to Kelvin" << endl;
     cout << "2.Celsius to Fahrenheit" << endl;
@@ -35,21 +35,21 @@ int main() {
             cin >> temperature;
             double celsiusToKelvin = temperature + 273.15;
             cout << temperature << "\370C in Kelvin is " << celsiusToKelvin << "K \n" << endl;
-            goto N;
+            goto N; // Program will jump to the label N
         }
     else if (choice == 2) {
             cout << "Enter temperature in Celsius: ";
             cin >> temperature;
             double celsiusToFahrenheit = (temperature * (9.0 / 5.0) + 32.0);
             cout << temperature << "\370C in Fahrenheit is " << celsiusToFahrenheit << "\370F \n" << endl;
-            goto N;
+            goto N; // Program will jump to the label N
         }
     else if (choice == 3) {
             cout << "Enter temperature in Kelvin: ";
             cin >> temperature;
             double kelvinToCelsius = temperature - 273.15;
             cout << temperature << "K in Celsius is " << kelvinToCelsius << "\370C \n" << endl;
-            goto N;
+            goto N; // Program will jump to the label N
 
         }
     else if (choice == 4) {
@@ -57,14 +57,14 @@ int main() {
             cin >> temperature;
             double kelvinToFahrenheit = (temperature - 273.15) * (9.0 / 5.0) - 32;
             cout << temperature << "K in Fahrenheit is " << kelvinToFahrenheit << "\370F \n" << endl;
-            goto N;
+            goto N; // Program will jump to the label N
         }
     else if (choice == 5) {
             cout << "Enter temperature in Fahrenheit: ";
             cin >> temperature;
             double fahrenheitToCelsius = (temperature - 32) * (5.0 / 9.0);
             cout << temperature << "\370F in Celsius " << fahrenheitToCelsius << "\370C \n" << endl;
-            goto N;
+            goto N; // Program will jump to the label N
         }
 
     else if (choice == 6) {
@@ -72,9 +72,9 @@ int main() {
             cin >> temperature;
             double fahrenheitToKelvin = (temperature - 32) * (5.0 / 9.0) + 273.15;
             cout << temperature << "\370F in Kelvin " << fahrenheitToKelvin << "K \n" << endl;
-            goto N;
+            goto N; // Program will jump to the label N
         }
-
+    // Ends program
     else if(choice == 7){
             cout << "Program has ended!" << endl;
         }
