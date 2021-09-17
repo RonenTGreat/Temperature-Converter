cf31ce1845f62b816266d5ae5f8191ab77122164
ronentgreat <ronenh53@gmail.com>
2021-09-17 11:37:26 +0000

    added a goto statement.

--- main.cpp
+++ main.cpp
@@ -43,38 +43,42 @@ int main() {
             double celsiusToFahrenheit = (temperature * (9.0 / 5.0) + 32.0);
             cout << temperature << "\370C in Fahrenheit is " << celsiusToFahrenheit << "\370F \n" << endl;
             goto N;
-        } else if (choice == 3) {
+        }
+    else if (choice == 3) {
             cout << "Enter temperature in Kelvin: ";
             cin >> temperature;
             double kelvinToCelsius = temperature - 273.15;
             cout << temperature << "K in Celsius is " << kelvinToCelsius << "\370C \n" << endl;
             goto N;
 
-        } else if (choice == 4) {
+        }
+    else if (choice == 4) {
             cout << "Enter temperature in Kelvin: ";
             cin >> temperature;
             double kelvinToFahrenheit = (temperature - 273.15) * (9.0 / 5.0) - 32;
             cout << temperature << "K in Fahrenheit is " << kelvinToFahrenheit << "\370F \n" << endl;
             goto N;
-        } else if (choice == 5) {
+        }
+    else if (choice == 5) {
             cout << "Enter temperature in Fahrenheit: ";
             cin >> temperature;
             double fahrenheitToCelsius = (temperature - 32) * (5.0 / 9.0);
             cout << temperature << "\370F in Celsius " << fahrenheitToCelsius << "\370C \n" << endl;
             goto N;
         }
-        else if (choice == 6) {
+
+    else if (choice == 6) {
             cout << "Enter temperature in Fahrenheit: ";
             cin >> temperature;
             double fahrenheitToKelvin = (temperature - 32) * (5.0 / 9.0) + 273.15;
             cout << temperature << "\370F in Kelvin " << fahrenheitToKelvin << "K \n" << endl;
             goto N;
         }
-        else if(choice == 7){
-            cout << "Program has ended" << endl;
-        }
 
-        else{
+    else if(choice == 7){
+            cout << "Program has ended!" << endl;
+        }
+    else{
             cout << "Enter a valid input please." << endl;
         }
 
